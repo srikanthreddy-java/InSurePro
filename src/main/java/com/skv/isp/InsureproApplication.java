8983111e5ddb8534c21c90c203bdc7131022fa76
SrikanthReddy Maram <srikanthreddy.maram@outlook.com>
2025-07-22 12:16:40 +0530

    Initial Commit

---
+++ src/main/java/com/skv/isp/InsureproApplication.java
@@ -0,0 +1,13 @@
+package com.skv.isp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsureproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsureproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skv/isp/InsureproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skv.isp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsureproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
